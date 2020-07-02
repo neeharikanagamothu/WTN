d35686d1f43c0080819adb6d3ef5a18de8cf1d0e
neeharikanagamothu <45072858+neeharikanagamothu@users.noreply.github.com>
2020-07-02 09:23:24 +0530

    Update LanguageBasicsEx2.java

--- LanguageBasicsEx2.java
+++ LanguageBasicsEx2.java
@@ -1,3 +1,6 @@
+/*Write a Program to accept a String as a command line argument and print a Welcome message as given below.
+ *Example1)C:\> java Sample JohnO/P Expected : Welcome John
+ */
 public class LanguageBasicsEx2 {
 public static void main(String[] args) 
 {
