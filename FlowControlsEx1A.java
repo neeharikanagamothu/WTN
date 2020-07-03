bf24b59cb576a1bb6a701003811be29be4c9478b
neeharikanagamothu <45072858+neeharikanagamothu@users.noreply.github.com>
2020-07-03 09:15:44 +0530

    Update FlowControlsEx1A.java

--- FlowControlsEx1A.java
+++ FlowControlsEx1A.java
@@ -1,3 +1,4 @@
+//Write a program to check if a given integer number is Positive, Negative, or Zero.
 public class FlowControlsEx1A {
 public static void main(String[] args) {
 	int a=Integer.parseInt(args[0]);
