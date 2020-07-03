4c94cf57f7ffb4d461ad87ffdc9dd427922eec95
neeharikanagamothu <45072858+neeharikanagamothu@users.noreply.github.com>
2020-07-03 09:16:53 +0530

    Update FlowControlsEx1B.java

--- FlowControlsEx1B.java
+++ FlowControlsEx1B.java
@@ -1,3 +1,6 @@
+/*Given two non-negative int values, print true if they have the same last digit, such as with 27 and 57. 
+ *lastDigit(7, 17) → true lastDigit(6, 17) → false lastDigit(3, 113) → true
+ */
 public class FlowControlsEx1B {
 public static void main(String[] args) {
 	int a=Integer.parseInt(args[0]);
