62d18e794e93246e736e95e1ae50baed7b0771a9
neeharikanagamothu <45072858+neeharikanagamothu@users.noreply.github.com>
2020-07-03 18:38:13 +0530

    Update FlowControlsEx4.java

--- FlowControlsEx4.java
+++ FlowControlsEx4.java
@@ -1,3 +1,7 @@
+/*Initialize two character variables in a program and display the characters in alphabetical order.
+ *Example1) if the first character is 's' and second character is 'e' then the output should be e,s
+ *Example2) if the first character is 'a' and second character is 'e', then the output should be a,e
+ */
 public class FlowControlsEx4 {
 public static void main(String[] args) {
 	String abc=args[0];
